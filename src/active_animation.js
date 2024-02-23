a0b44c2c170e0dfcad3d9c798c20d4b24980a31d
JinKyu <rlawlsrb167@naver.com>
2024-02-24 00:40:04 +0900

    Feat: JS scroll시section 별로  animation 추가

---
+++ src/active_animation.js
@@ -0,0 +1,56 @@
+const sectionIds = ["#home", "#about", "#skills", "#projects", "#contact"];
+const sections = sectionIds.map((id) => document.querySelector(id));
+
+const options = {
+  rootMargin: "10% 0px 0px 0px",
+  threshold: 0.2,
+};
+const observer = new IntersectionObserver(observerCallback, options);
+
+sections.forEach((section) => observer.observe(section));
+
+function observerCallback(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      switch (entry.target.id) {
+        case "home":
+          entry.target.classList.add("active");
+          entry.target.querySelector(".sun").classList.add("active");
+          entry.target.querySelector(".home_avatar").classList.add("active");
+          break;
+        case "about":
+          entry.target
+            .querySelectorAll(".info_item")
+            .forEach((item) => item.classList.add("active"));
+          break;
+        case "skills":
+          entry.target
+            .querySelectorAll(".bar_value")
+            .forEach((item) => item.classList.add("active"));
+          break;
+        default:
+          break;
+      }
+    } else {
+      switch (entry.target.id) {
+        case "home":
+          entry.target.classList.remove("active");
+          entry.target.querySelector(".sun").classList.remove("active");
+          entry.target.querySelector(".home_avatar").classList.remove("active");
+          break;
+        case "about":
+          entry.target
+            .querySelectorAll(".info_item")
+            .forEach((item) => item.classList.remove("active"));
+          break;
+        case "skills":
+          entry.target
+            .querySelectorAll(".bar_value")
+            .forEach((item) => item.classList.remove("active"));
+          break;
+        default:
+          break;
+      }
+    }
+  });
+}
